6a1c9ef44763ceaa5d5f20323f61d46770f66a33
gabste8831 <gabrielsteffens2003@gmail.com>
2026-03-10 07:28:49 -0300

    implementação de tratamento de erros (middlewares)

---
+++ src/models/Livro.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const livroSchema = new mongoose.Schema(
+  {
+    id: {type: String},
+    titulo: {type: String, required: [true, "O título do livro é obrigatório."]},
+    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O autor do livro é obrigatório."]},
+    editora: {type: String, required: [true, "A editora do livro é obrigatória."]},
+    numeroPaginas: {type: Number}
+  }
+);
+
+const livros= mongoose.model("livros", livroSchema);
+
+export default livros;
